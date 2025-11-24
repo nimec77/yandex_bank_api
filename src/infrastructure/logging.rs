ffda6fafbb0edc7705e5a8678a30ba2118d55b7d
Dmitry Seloustev <d.seloustev@adguard.com>
2025-11-24 21:02:14 +0700

    enhance logging: improve log output format by including target, file name, and line number

--- src/infrastructure/logging.rs
+++ src/infrastructure/logging.rs
@@ -1,12 +1,19 @@
-use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt, fmt};
 
 pub fn init_logging() {
     // Set default log level to "info" if RUST_LOG is not set
+    // RUST_LOG format: "module::path=level,other_module=level"
+    // Example: "RUST_LOG=actix_web=info,yandex_bank_api=debug"
     let filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new("info"));
     
     tracing_subscriber::registry()
         .with(filter)
-        .with(tracing_subscriber::fmt::layer())
+        .with(
+            fmt::layer()
+                .with_target(true)  // Show module/target in logs
+                .with_file(true)    // Show file name
+                .with_line_number(true)  // Show line number
+        )
         .init();
 }
